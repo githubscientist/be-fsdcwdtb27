745bc05c63a6683a122f0655303938e63fc34b2f
githubscientist <sathish.datascientist@gmail.com>
2025-07-23 20:45:12 +0530

    getAllTodos --done

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,17 @@
+const Todo = require("../models/todo");
+
 // create a todoController object to wrap all the todo-related functions
 const todoController = {
-    getAllTodos: (req, res) => {
-        res.json({ message: 'Hello, Todo!' });
+    getAllTodos: async (req, res) => {
+        // get all the todos from the database
+        const todos = await Todo.find();
+
+        // send the todos as a response
+        res
+            .status(200)
+            .send({
+                todos: todos,
+            });
     }
 }
 
