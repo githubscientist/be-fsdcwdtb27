45bfebc53d08e6a7e706c70cbf7fc8bea6d5ff6c
githubscientist <sathish.datascientist@gmail.com>
2025-07-23 20:01:35 +0530

    config utility added

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const { default: mongoose } = require("mongoose");
 const app = require("./app");
-require('dotenv').config();
+const { MONGODB_URI, PORT } = require("./utils/config");
 
 console.log('Connecting to the database...');
 // connect to the database
 mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(MONGODB_URI)
     .then(() => {
         console.log('Connected to the database!');
 
         console.log('Starting the server...');
         // run the application
-        app.listen(3001, () => {
-            console.log(`Server is running on http://localhost:3001`);
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
         });
     })
     .catch((err) => {
