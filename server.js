378d77cf013ed6b713249cbac86610ebd04a87a9
githubscientist <sathish.datascientist@gmail.com>
2025-07-23 19:56:31 +0530

    database connectivity done + dotenv configured

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+const { default: mongoose } = require("mongoose");
 const app = require("./app");
+require('dotenv').config();
 
-// run the application
-app.listen(3001, () => {
-    console.log(`Server is running on http://localhost:3001`);
-});
+console.log('Connecting to the database...');
+// connect to the database
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to the database!');
+
+        console.log('Starting the server...');
+        // run the application
+        app.listen(3001, () => {
+            console.log(`Server is running on http://localhost:3001`);
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to the database:', err.message);
+    })
