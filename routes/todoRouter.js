2ef4068e9ec17e2fc3827942c83be90c59a9ddcd
githubscientist <sathish.datascientist@gmail.com>
2025-07-29 20:11:22 +0530

    auth middleware added to getAllTodos

--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -1,12 +1,13 @@
 // import express module
 const express = require('express');
 const { getAllTodos, getTodoById, createTodo, updateTodo, deleteTodo } = require('../controllers/todoController');
+const auth = require('../middlewares/auth');
 
 // create a router object for managing todo routes
 const todoRouter = express.Router();
 
 // manage the routes for todos
-todoRouter.get('/', getAllTodos);
+todoRouter.get('/', auth.isAuthenticated, getAllTodos);
 todoRouter.get('/:id', getTodoById);
 todoRouter.post('/', createTodo);
 todoRouter.put('/:id', updateTodo);
